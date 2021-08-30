0fdfef9eb6f12ba791d3442d7714888485850b1d
AKSz3 <AKSz3@users.noreply.github.com>
2021-08-31 01:19:43 +0530

    Added cloud db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
 const connectDB = async() => {
-    const conn = await mongoose.connect(MONGODB_ATLAS_DEV, {
+    const conn = await mongoose.connect(process.env.MONGODB_ATLAS, {
         useNewUrlParser: true,
-        useCreateIndex: true,
         useUnifiedTopology: true,
-        useFindAndModify: true,
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`);
